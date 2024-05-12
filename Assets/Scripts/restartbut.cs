e7dd61505c24d455172be0c7db6e608dfb61a2f0
Ahmed Elkholy <ahmed.elkholy6480@gmail.com>
2024-05-12 07:24:09 +0300

    project

---
+++ Assets/Scripts/restartbut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartbut : MonoBehaviour
+{
+    public void RetryLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
+    }
+
+   
+
+    public void mainmenu()
+    {
+        SceneManager.LoadScene("menu"); 
+       
+
+}
+}
